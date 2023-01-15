cae5c0cf655bebbf56442559ff0670420fd34c28
gamseb <gamseb@gmail.com>
2023-01-16 00:21:18 +0100

    Cleaned up the code at flags/main.js and added comments

--- flags/static/flags/main.js
+++ flags/static/flags/main.js
@@ -1,11 +1,13 @@
+// Keeps track of selected buttons
 let selectedFlags = [];
 
 function filterByColor(color) {
+    // 1. Add black borders to button to show they are selected
     let btnId = `btn-flag-${color}`
     let btnElement = document.getElementById(btnId);
     btnElement.classList.toggle("active-btn-flag");
     togglePropertyInList(btnId)
-
+    // 2. Hide and un hide rows depending on the buttons pressed
     const elements = document.getElementsByClassName("flag-row");
     for (let i = 0; i < elements.length; i++) {
       const element = elements[i];
@@ -37,6 +39,7 @@ function resetFilters(){
 function togglePropertyInList(property) {
     // Remove the 'btn-' part of the class
     property = property.slice(4)
+    // If property is not present in the list add it, if it is then remove it
     if (selectedFlags.includes(property)) {
         selectedFlags = selectedFlags.filter(arrayItem => arrayItem !== property);
     } else {
@@ -45,7 +48,6 @@ function togglePropertyInList(property) {
 }
 
 function isFlagSelected(flagElement) {
+    // Compare the list of pressed buttons with the colors on the flag and see if they are all present
     return selectedFlags.every(element => Array.from(flagElement.classList).includes(element));
 }
-
-Array.from(document.getElementsByClassName("btn-flag")).forEach(btn => btn.addEventListener("click", filterByColor));
