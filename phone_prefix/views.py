54a75f2181415878ef68f158be6103c9d98e5ec6
gamseb <gamseb@gmail.com>
2023-01-15 06:39:09 +0100

    changed name of file variable

--- phone_prefix/views.py
+++ phone_prefix/views.py
@@ -8,12 +8,10 @@ import json
 
 def phone_prefix(request):
     print(os.getcwd())
-    with open('phone_prefix/resources/countries.json') as countries:
-        countries = json.load(countries)
+    with open('phone_prefix/resources/countries.json') as file:
+        countries = json.load(file)
 
     context = {
         'countries': countries
     }
-    print(context)
-
     return render(request, 'phone_prefix/phone_prefix.html', context)
